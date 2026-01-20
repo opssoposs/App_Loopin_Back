a26a37395daa67102f3d4d6c902d5201dc1db9e7
opssoposs <boemjunson4@gmail.com>
2026-01-21 06:49:44 +0900

    loopin-backend

---
+++ loopin_backend/src/main/kotlin/com/loopin/backend/loopin_backend/LoopinBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.loopin.backend.loopin_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoopinBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoopinBackendApplication>(*args)
+}
---
+++ loopin_backend/src/test/kotlin/com/loopin/backend/loopin_backend/LoopinBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.loopin.backend.loopin_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LoopinBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
